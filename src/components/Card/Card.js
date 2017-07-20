0d356ca595a3e1605881b26870279e3e57679c31
Noy <60noypearl@gmail.com>
2017-07-20 22:30:42 +0300

    fixed images problem

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 import Image from '../Image/Image.js';
 import style from './style.css';
-import {Col} from 'react-bootstrap';
 import design from '../../utils/design.js';
 const{primary_color,material_red,secondary_color} = design;
 // import deleteImage from '../../images/ic_delete';
@@ -16,7 +15,6 @@ export default class Card extends Component{
 
     render() {
       return (
-        <Col xs={6} md={3} lg={2}>
         <div className={style.container}>
           <Image imageSrc={this.props.image}/>
           <div className={style.bottomBar}>
@@ -27,7 +25,6 @@ export default class Card extends Component{
             <button onClick={this.props.deleteHandler} className={style.btnDelete}/>
           </div>
         </div>
-        </Col>
       );
     }
   }
