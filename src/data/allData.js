76ce8041d8c56dc6628cbacc74639c54945a5ed7
Noy <60noypearl@gmail.com>
2017-07-21 11:29:15 +0300

    Updates

--- src/data/allData.js
+++
@@ -1,6 +0,0 @@
-import groupsData from './groups.json';
-import imagesData from './images.json';
-import tagsData from './tags.json';
-import usersData from './users.json';
-
-export {groupsData,imagesData,tagsData,usersData};
